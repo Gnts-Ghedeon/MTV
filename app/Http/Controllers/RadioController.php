46c98c989d6c0f1a950c9030ff3151b1680fe7ff
smeth <kanasmeth@gmail.com>
2023-12-20 19:24:55 +0300

    ajout ctlr radio et podcast

---
+++ app/Http/Controllers/RadioController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Radios;
+use Illuminate\Http\Request;
+
+class RadioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Radios  $radios
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Radios $radios)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Radios  $radios
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Radios $radios)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Radios  $radios
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Radios $radios)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Radios  $radios
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Radios $radios)
+    {
+        //
+    }
+}
